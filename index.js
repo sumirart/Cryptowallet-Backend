dd294c0dc0ba303396d75f3a8da14a02760a8ba4
Pratama Sumirat <akun.pratamasumirat@gmail.com>
2021-12-16 00:53:23 +0700

    menambahkan beberapa endpoint

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
+
+app.get("/halo", (req, res) => {
+  res.send("Halo semua semuanya!");
+});
+
+app.get("/json", (req, res) => {
+  res.json({ pesan: "Mengirim Format JSON" });
+});
+
+app.post("/login", (req, res) => {
+  res.status(201).json({ method: "POST", message: "Berhasil login" });
+});
+
+app.delete("/account", (req, res) => {
+  res
+    .status(200)
+    .json({ method: "DELETE", message: "Berhasil menghapus akun" });
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
